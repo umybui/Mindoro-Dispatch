808ff8d47b5fa5938db02706571f03915f0dc531
umybui <dennisumlas@gmail.com>
2026-09-10 10:24:48 +0800

    Create 02_Mainland_Palawan.py

---
+++ pages/02_Mainland_Palawan.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mainland Palawan Dispatch Dashboard",
+    layout="wide"
+)
+
+st.title("Mainland Palawan Dispatch Dashboard")
+
+st.info("Palawan dashboard under development.")
