a6a8d8f1371c8fd6845020543c3a573766ad8cc7
umybui <dennisumlas@gmail.com>
2026-09-11 11:28:48 +0800

    Update 03_Mainland_Catanduanes.py

--- pages/03_Mainland_Catanduanes.py
+++ pages/03_Mainland_Catanduanes.py
@@ -33,18 +33,18 @@ def load_data():
     )
 
     df.rename(
-    columns={
-        "Power Plant": "Plant"
-    },
-    inplace=True
-)
-    
-df["Value"] = (
-    pd.to_numeric(
-        df["Value"],
-        errors="coerce"
-    ) / 1000
-)
+        columns={
+            "Power Plant": "Plant"
+        },
+        inplace=True
+    )
+
+    df["Value"] = (
+        pd.to_numeric(
+            df["Value"],
+            errors="coerce"
+        ) / 1000
+    )
 
     return df
 
