d1e01b24db290541323be0d671d9dcba4b3531b1
umybui <dennisumlas@gmail.com>
2026-09-11 10:27:25 +0800

    Update 03_Mainland_Catanduanes.py

--- pages/03_Mainland_Catanduanes.py
+++ pages/03_Mainland_Catanduanes.py
@@ -32,6 +32,13 @@ def load_data():
         errors="coerce"
     )
 
+    df.rename(
+    columns={
+        "Power Plant": "Plant"
+    },
+    inplace=True
+)
+    
     df["Value"] = pd.to_numeric(
     df["Value"],
     errors="coerce"
@@ -40,8 +47,7 @@ def load_data():
     return df
 
 df = load_data()
-st.write(df.columns.tolist())
-st.stop()
+
 # Optional manual refresh
 if st.sidebar.button("Refresh Data"):
     st.cache_data.clear()
