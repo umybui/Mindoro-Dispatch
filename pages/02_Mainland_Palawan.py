afba814e6c138a97d5b14e8fba50250f2e288b32
umybui <dennisumlas@gmail.com>
2026-09-18 14:22:36 +0800

    Update 02_Mainland_Palawan.py

--- pages/02_Mainland_Palawan.py
+++ pages/02_Mainland_Palawan.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 from streamlit_sortables import sort_items
 
 # =====================================================
